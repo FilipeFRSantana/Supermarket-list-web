c29c37d6d5ae498e092e4ad71aa3a1f64e552a27
FilipeFRSantana <lipecleo@hotmail.com>
2024-06-24 16:02:21 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {HomeScreen} from './Screens/Home'
+
+const fakelist = [
+  {
+    id: 0,
+    name: "Arroz",
+    quantity: 2,
+    checked: false,
+  },
+  {
+    id: 1,
+    name: "Feijão",
+    quantity: 3,
+    checked: false,
+  },
+  {
+    id: 2,
+    name: "Macarrão",
+    quantity: 5,
+    checked: false,
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HomeScreen/>
   );
 }
 
---
+++ src/Screens/Home/index.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import "./index.css";
+import { Button, Input } from "../../components";
+
+export const HomeScreen = () => {
+  const [username, setUsername] = useState("");
+  return (
+    <div className="home-screen-container">
+      <div className="home-screen-content-container">
+        <img
+          className="shopping-bag-image"
+          src="/images/shopping-bag.svg"
+          alt="imagem shopping bag"
+        />
+        <h1 className="home-screen-title">
+          Sua lista de supermercado mais fácil do que nunca
+        </h1>
+        <h3 className="home-screen-subtitle">
+          Ajudamos você a organizar sua lista de compras de forma descomplicada.
+        </h3>
+        <h3 className="home-screen-subtitle-description">
+          Digite abaixo seu usuário para ter acesso a sua lista de compras
+        </h3>
+        <Input
+          onChange={(text) => setUsername(text)}
+          value={username}
+          label="UserName"
+          placeholder="Ex: usuario1"
+        />
+        <div className="home-screen-button-container">
+          <Button>Acessar</Button>
+        </div>
+      </div>
+    </div>
+  );
+};
