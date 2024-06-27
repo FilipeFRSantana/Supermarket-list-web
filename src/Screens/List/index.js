fc01ebd8ea0661647cf5522de5d8f1f1c69e7ea9
FilipeFRSantana <lipecleo@hotmail.com>
2024-06-27 13:38:59 -0300

    feat: create final layout list

--- src/Screens/List/index.js
+++ src/Screens/List/index.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import "./index.css";
 import { getList } from "../../services/request";
-import { ListCard } from "../../components";
+import { Button, ListRender, Loader } from "../../components";
 
 export const ListScreen = () => {
   const [loading, setloading] = useState(true);
@@ -21,23 +21,20 @@ export const ListScreen = () => {
     <div className="list-screen-container">
       <div className="list-screen-content-container">
         <div className="list-screen-header">
-          <img
-            className="image-logo"
-            src="/images/logo.png"
-            alt="imagem logo"
-          />
-          <h1>Lista Supermercado</h1>
+          <div className="list-screen-title-container">
+            <img
+              className="image-logo"
+              src="/images/logo.png"
+              alt="logo supermarket"
+            />
+            <h1 className="list-screen-header-title"> Lista Supermercado </h1>
+          </div>
+          <div className="list-screen-header-button-container">
+          <Button>ADICIONAR</Button>
+          </div>
         </div>
         <div className="list-screen-list-container">
-          {loading && <h3>Carregando...</h3>}
-          {!loading && listData?.length > 0 ? (
-            listData.map((item) => <ListCard key={item._id} item={item} />)
-          ) : (
-            <h3>
-              Sua lista está vazia, adicione um novo item clicando no botão de
-              "ADICIONAR"!
-            </h3>
-          )}
+          {loading ? <Loader /> : <ListRender list={listData} />}
         </div>
       </div>
     </div>
